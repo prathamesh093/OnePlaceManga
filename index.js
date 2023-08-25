c2ed1b19df4cecb2bf07363f49fda40017b237bf
Mayur <mayurbehere7432@gmail.com>
2023-08-25 17:14:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// $(document).on("scroll", function(){
+//     if
+//   ($(document).scrollTop() > 86){
+//       $("#banner").addClass("shrink");
+//     }
+//     else
+//     {
+//         $("#banner").removeClass("shrink");
+//     }
+// });
+
