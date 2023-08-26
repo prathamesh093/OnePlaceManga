2d1b0180839c7ec1c30acbcde19c0a658f949f95
Mayur <mayurbehere7432@gmail.com>
2023-08-26 15:58:49 +0530

    first page done

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+const searchbar = document.getElementById("searchbar");
+const searchInput = searchbar.querySelector("input[type='search']");
 
-// $(document).on("scroll", function(){
-//     if
-//   ($(document).scrollTop() > 86){
-//       $("#banner").addClass("shrink");
-//     }
-//     else
-//     {
-//         $("#banner").removeClass("shrink");
-//     }
-// });
+searchbar.addEventListener("mouseenter", function() {
+searchbar.style.opacity = "1";
+});
+searchbar.addEventListener("mouseleave", function() {
+searchbar.style.opacity = "0.5";
+});
+searchbar.addEventListener("click", function() {
+ searchbar.style.opacity = "1";
+});
+searchInput.addEventListener("input", function() {
+ 
+    searchbar.style.opacity = "1";
+});
+searchInput.addEventListener("blur", function() {
+        searchbar.style.opacity = "0.5";
+});
 
+
+
+
+$( document ).ready(function() {
+    // Bounce button
+    $("#animatebutton").click(function(){
+        const element =  document.querySelector('.animatebutton');
+        element.classList.add('animated', 'pulse');
+        setTimeout(function() {
+          element.classList.remove('pulse'); 
+  },        1000);
+    });
+    
+    
+});
